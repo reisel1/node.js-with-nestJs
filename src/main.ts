3c9038f55097acb1758428edca141511e68d8317
reisel1 <reisy04@gmail.com>
2022-04-27 10:27:49 +0300

    add openapi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -9,6 +10,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe()); //{disableErrorMessages: true,}
+
+  const config = new DocumentBuilder()
+    .setTitle('Items example')
+    .setDescription('The items API description')
+    .setVersion('1.0')
+    .addTag('Items')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(7000);//process.env.PORT
 }
 bootstrap();
